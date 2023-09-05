faa8ae8d4cb6adbdef08cf19ad2feb91ceec1f1e
Prasanth P <iprasanthpraba@gmail.com>
2023-09-05 22:06:02 +0530

    adding drawer

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import "./App.css";
+import MiniDrawer from "./Drawer";
 
 function App() {
   return (
     <>
+    <MiniDrawer/>
       <Stack spacing={2} direction="row">
         <Button variant="text">Text</Button>
         <Button variant="contained">Contained</Button>
