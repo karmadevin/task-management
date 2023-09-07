5069bd46e317e77277dda7e2dac3862d8f783df1
Prasanth P <iprasanthpraba@gmail.com>
2023-09-07 12:04:44 +0530

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,14 @@
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
-import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
+import { Paper } from "@mui/material";
 import "./App.css";
-import MiniDrawer from "./Drawer";
+import Home from "./Home";
 
 function App() {
   return (
-    <>
-    <MiniDrawer/>
-      <Stack spacing={2} direction="row">
-        <Button variant="text">Text</Button>
-        <Button variant="contained">Contained</Button>
-        <Button variant="outlined">Outlined</Button>
-      </Stack>
-      <Box
-        component="form"
-        sx={{
-          "& > :not(style)": { m: 1, width: "25ch" },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        <TextField id="filled-basic" label="Filled" variant="filled" />
-        <TextField id="standard-basic" label="Standard" variant="standard" />
-      </Box>
-    </>
+    <Paper elevation={4} style={{ minHeight:"100vh", borderRadius: "0px"}}>
+      <div className="App">
+        <Home/>
+      </div>
+    </Paper>
   );
 }
 
