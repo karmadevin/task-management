d553fc79a031a36b1bd53cc4c52ae15b4f81d8db
shanmitha <sanmithasree22@gmail.com>
2023-09-11 22:23:27 +0530

    Validation and CSS tweaks for login page

--- src/Login.jsx
+++ src/Login.jsx
@@ -9,12 +9,14 @@ import HeroImage from "./assets/HeroImage.svg";
 import TextField from "@mui/material/TextField";
 import Checkbox from "@mui/material/Checkbox";
 import Button from "@mui/material/Button";
+import { Formik } from "formik";
 
 const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
 const Login = () => {
+
+
   return (
-    
     <div className="Login">
       <div>
         <Box
@@ -37,100 +39,174 @@ const Login = () => {
           }}
         >
           <div className="Box">
-              <Box className="Field">
-                <p>Sign in to your account</p>
-                <div className="Field1" style={{ textAlign: "center",
-                width: "350px" }}>
-                  <h4
-                    style={{
-                      textAlign: "left",
-                      width: "60px",
-                      marginTop: "10px",
-                      marginLeft: "-1px",
-                    }}
-                  >
-                    Email
-                  </h4>
-                  <TextField
-                  className="textfield"
-                    label="Admin@gmail.com"
-                    variant="outlined"
-                  />
-                </div>
+            <Box className="Field">
+              <p>Sign in to your account</p>
+              <Formik
+                initialValues={{ email: "", password: "" }}
+                validate={(values) => {
+                  const errors = {};
+                  if (!values.email) {
+                    errors.email = "Required";
+                  } else if (
+                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(
+                      values.email
+                    )
+                  ) {
+                    errors.email = "Invalid email address";
+                  }
+                  return errors;
+                }}
+                onSubmit={(values, { setSubmitting }) => {
+                  setTimeout(() => {
+                    alert(JSON.stringify(values, null, 2));
+                    setSubmitting(false);
+                  }, 400);
+                }}
+              >
+                {({
+                  values,
+                  errors,
+                  touched,
+                  handleChange,
+                  handleSubmit
+                }) => (
+                  <form onSubmit={handleSubmit}>
+                    <div
+                      className="Field1"
+                      style={{ textAlign: "center", width: "350px" }}
+                    >
+                      <h4
+                        style={{
+                          textAlign: "left",
+                          width: "60px",
+                          marginTop: "10px",
+                          marginLeft: "-1px",
+                        }}
+                      >
+                        Email
+                      </h4>
+                      <TextField
+                        type="email"
+                        name="email"
+                        className="textfield"
+                        label="Admin@gmail.com"
+                        variant="outlined"
+                        onChange={handleChange}
+                        value={values.email}
+                        required
+                      />
+                      {errors.email && touched.email && errors.email}
+                    </div>
 
-                <div className="Field1" style={{ textAlign: "center", width: "350px" }}>
-                  <h4
-                    style={{
-                      textAlign: "left",
-                      width: "60px",
-                      marginTop: "30px",
-                      marginLeft: "-1px",
-                    }}
-                  >
-                    Password
-                  </h4>
-                  <TextField
-                    className="textfield"
-                    label="Enter Your Password"
-                    variant="outlined"
-                  />
-                  <div className="alignment">
-                    <h6>
-                      Remember me <Checkbox {...label} />
-                    </h6>
-                    <h6>Forgot Password?</h6>
-                  </div>
-                </div>
+                    <div
+                      className="Field1"
+                      style={{ textAlign: "center", width: "350px" }}
+                    >
+                      <h4
+                        style={{
+                          textAlign: "left",
+                          width: "60px",
+                          marginTop: "30px",
+                          marginLeft: "-1px",
+                        }}
+                      >
+                        Password
+                      </h4>
+                      <TextField
+                        name="password"
+                        type="password"
+                        onChange={handleChange}
+                        value={values.password}
+                        className="textfield"
+                        label="Enter Your Password"
+                        variant="outlined"
+                        required
+                      />
+                      {errors.password && touched.password && errors.password}
+                      <div className="alignment">
+                        <h6>
+                          Remember me <Checkbox {...label} />
+                        </h6>
+                        <a href="#">Forgot Password?</a>
+                      </div>
+                    </div>
 
-                <div className="Login-Btns">
-                  <div className="login-btn">
-                    <Button
-                      variant="contained"
-                      style={{ background: "#ec3d50", width: "350px",height:"50px",borderRadius:"10px" }}
-                    >
-                      Login as Admin
-                    </Button>
-                  </div>
-                  <div className="login-btn">
-                    <Button
-                      variant="contained"
-                      style={{ background: "#ec3d50", width: "350px",height:"50px",borderRadius:"10px"  }}
-                    >
-                      Login as Team Manager
-                    </Button>
-                  </div>
-                  <div className="login-btn">
-                    <Button
-                      variant="contained"
-                      style={{ background: "#ec3d50", width: "350px",height:"50px",borderRadius:"10px"  }}
-                    >
-                      Login in as Developer
-                    </Button>
-                  </div>
-                  <div className="login-btn">
-                    <Button
-                      variant="contained"
-                      style={{ background: "#ec3d50", width: "350px",height:"50px",borderRadius:"10px"  }}
-                    >
-                      Login as Client
-                    </Button>
-                  </div>
-                </div>
-              </Box>
+                    <div className="Login-Btns">
+                      <div className="login-btn">
+                        <a href="#">
+                          <Button
+                            type="submit"
+                            variant="contained"
+                            style={{
+                              background: "#ec3d50",
+                              width: "350px",
+                              height: "50px",
+                              borderRadius: "10px",
+                            }}
+                          >
+                            Login as Admin
+                          </Button>
+                        </a>
+                      </div>
+                      <div className="login-btn">
+                        <a href="#">
+                          <Button
+                            variant="contained"
+                            style={{
+                              background: "#ec3d50",
+                              width: "350px",
+                              height: "50px",
+                              borderRadius: "10px",
+                            }}
+                          >
+                            Login as Team Manager
+                          </Button>
+                        </a>
+                      </div>
+                      <div className="login-btn">
+                        <a href="#">
+                          <Button
+                            variant="contained"
+                            style={{
+                              background: "#ec3d50",
+                              width: "350px",
+                              height: "50px",
+                              borderRadius: "10px",
+                            }}
+                          >
+                            Login in as Developer
+                          </Button>
+                        </a>
+                      </div>
+                      <div className="login-btn">
+                        <a href="#">
+                          <Button
+                            variant="contained"
+                            style={{
+                              background: "#ec3d50",
+                              width: "350px",
+                              height: "50px",
+                              borderRadius: "10px",
+                            }}
+                          >
+                            Login as Client
+                          </Button>
+                        </a>
+                      </div>
+                    </div>
+                  </form>
+                )}
+              </Formik>
+            </Box>
           </div>
           <h3 className="copyright">Copyright @2023KarmaDev.in.</h3>
         </Box>
       </div>
-      <img className="doodle1" src={doo1}/>
-      <img className="doodle2" src={doo2}/>
-      <img className="doodle3" src={doo3}/>
+      <img className="doodle1" src={doo1} />
+      <img className="doodle2" src={doo2} />
+      <img className="doodle3" src={doo3} />
     </div>
-    
-    
-  
-    );
+  );
 };
 
 export default Login;
-
-
