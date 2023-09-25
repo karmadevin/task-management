8eac7ad005bfea32ccdf1c22611b643ef492b623
GOKUL PRSASATH <prasathgokul19@gmail.com>
2023-09-25 11:06:49 +0530

    ClientDashboard,ClientTask,ClientProject Files

---
+++ src/features/clientDashBoard/Clientcard.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import "./Clientcard.css"
+import taskicon from "./icons/taskicon.svg"
+
+import proicon from './icons/proicon.svg'
+
+function Clientcard() {
+  return (
+  
+
+    <div className='mainbox'>
+    <div className='containerbox'>
+      <div className='child1'>
+        <img src={proicon} alt="Karmadev.in" className='project'/>
+      </div>
+      <div className='child2'>
+        <p className='child2text'>
+          Total Projects
+        </p >
+        <h5 className='child2text'>
+          499
+        </h5>
+      </div>
+    </div>
+    <div className='containerbox'>
+      <div className='child1'>
+        <img src={taskicon} alt="Karmaev.in" className='task'/>
+      </div>
+      <div className='child2'>
+        <p className='child2text'>
+          Total Tasaks
+        </p >
+        <h5 className='child2text'>
+          499
+        </h5>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default Clientcard;
---
+++ src/features/clientProject/ProjectHeading.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "./ProjectHeading.css";
+import { Box, Breadcrumbs, Button, Link, Typography } from "@mui/material";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import SearchIcon from "@mui/icons-material/Search";
+import Person2OutlinedIcon from "@mui/icons-material/Person2Outlined";
+import FilterAltOutlinedIcon from "@mui/icons-material/FilterAltOutlined";
+
+const ProjectHeading = () => {
+  return (
+    <div className="superadminproject-heading">
+      <div className="superadminproject-head-content1">
+        <h2>Project</h2>
+        <Box className="superadminproject-box">
+          <Breadcrumbs
+            className="superadminproject-breadcrumbs"
+            aria-label="breadcrumb"
+            separator={<NavigateNextIcon fontSize="small" />}
+          >
+            <Link underline="hover" color="rgb(159, 165, 165)" href="#">
+              SUPER ADMIN
+            </Link>
+            <Typography className="superadminproject-typography">
+              PROJECTS
+            </Typography>
+          </Breadcrumbs>
+        </Box>
+      </div>
+      <div className="superadminproject-head-content2">
+        <div className="superadminproject-search">
+          <div className="superadminproject-searchIcon">
+            <SearchIcon />
+          </div>
+          <input type="text" placeholder="search" />
+        </div>
+        <div className="superadminproject-profileIcon">
+          <Person2OutlinedIcon />
+        </div>
+        <div className="superadminproject-profileIcon">
+          <FilterAltOutlinedIcon />
+        </div>
+        <Button className="superadminproject-addproject">+ Add Project</Button>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default ProjectHeading;
